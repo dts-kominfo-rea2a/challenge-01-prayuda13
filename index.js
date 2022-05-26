f313c171507c31ca54c7833ca84d5a068c04ec48
Prayuda Sagita Wardana <wpruda@gmail.com>
2022-05-26 20:35:36 +0700

    Tugas struktur data

--- index.js
+++ index.js
@@ -8,6 +8,12 @@
 // isHavePet: String
 // education: Array
 // favoriteRestaurant: Array
+let favoriteRestaurant = new Set();
+
+let restaurant1 = { 1: 'Bento', 2: 'Sushi', 3: 'Pancake', 4: 'Eggy', 5: 'Tempura', 6: 'Bento', 7: 'Eggy', 8: 'Padang', 9: 'Tteok', 10: 'Sushi', 11: 'Sushi' };
+let restaurant2 = { 1: 'Tempura', 2: 'Bento', 3: 'Sushi', 4: 'Pancake', 5: 'Padang', 6: 'Katsu', 7: 'Geprek', 8: 'Pancake', 9: 'Eggy' };
+favoriteRestaurant.add(restaurant1);
+favoriteRestaurant.add(restaurant2);
 
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 const firstUser = {
@@ -22,7 +28,7 @@ const firstUser = {
     ['name: SMP 02', 'city: Jakarta', 'graduate: 2019'],
     ['name: SMA 03', 'city: Tanggerang'],
   ],
-  favoriteRestaurant: [['Bento'], ['Sushi'], ['Pancake'], ['Eggy'], ['Tempura'], ['Bento'], ['Eggy'], ['Padang'], ['Tteok'], ['Sushi'], ['Sushi']],
+  favoriteRestaurant,
 };
 const secondUser = {
   name: 'Wendy',
@@ -36,7 +42,7 @@ const secondUser = {
     ['name: SMP 03', 'city: Bogor', 'graduate: 2013'],
     ['name: SMA 01', 'city: Surabya', 'graduate: 2016'],
   ],
-  favoriteRestaurant: [['Tempura'], ['Bento'], ['Sushi'], ['Pancake'], ['Padang'], ['Katsu'], ['Geprek'], ['Pancake'], ['Eggy']],
+  favoriteRestaurant,
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
