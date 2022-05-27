eae2af164fbe7939e8b7041578cb2eb74c538cd7
Prayuda Sagita Wardana <wpruda@gmail.com>
2022-05-27 21:17:12 +0700

    Tugas struktur data

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const firstUser = {
   gender: 'Female',
   age: 17,
   email: 'monica@dingdong.com',
-  favoriteColor: ['Yellow', 'Pink', 'White', 'Purple'],
+  favoriteColor: Array.from(new Set(['Yellow', 'Pink', 'White', 'Purple'])),
   isHavePet: 'Yes',
   education: [
     { name: 'SD 01', city: 'Jakarta', graduate: '2016' },
     { name: 'SMP 02', city: 'Jakarta', graduate: '2019' },
     { name: 'SMA 03', city: 'Tangerang' },
   ],
-  favoriteRestaurant: new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']),
+  favoriteRestaurant: Array.from(new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi'])),
 };
 const secondUser = {
   name: 'Wendy',
   gender: 'Male',
   age: 23,
   email: 'wendy@dingdong.com',
-  favoriteColor: ['Blue', 'Black', 'Gray'],
+  favoriteColor: Array.from(new Set(['Blue', 'Black', 'Grey'])),
   isHavePet: 'No',
   education: [
     { name: 'SD 02', city: 'Jakarta', graduate: '2010' },
@@ -28,7 +28,7 @@ const secondUser = {
     { name: 'SMA 01', city: 'Surabaya', graduate: '2016' },
     { name: 'Universitas Maju', city: 'Tangerang' },
   ],
-  favoriteRestaurant: new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']),
+  favoriteRestaurant: Array.from(new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy'])),
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
